d14e293dcd4ada9f4047f25bc0a87c04016a9940
DanSmith8011 <dansmithdan7@aol.co.uk>
2026-08-06 19:00:36 +0100

    Added environment variables for database URL security

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 from routes import books
 from database import engine
 from models import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(books.router)
 Base.metadata.create_all(bind=engine)
+
